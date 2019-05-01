71836aed7b19c30bdb8f92afca10ff44018a60f4
Yutaka <shimayu22@gmail.com>
2019-05-01 06:28:44 +0000

    7.3

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/about',to:'static_pages#about'
   get '/contact',to:'static_pages#contact'
   get '/signup',to:'users#new'
+  post '/signup',to:'user#create'
   resources :users
 end
