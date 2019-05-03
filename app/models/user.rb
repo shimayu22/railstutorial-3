4d456395b02da66c8754dc8a97a5e01b32f44130
Yutaka <shimayu22@gmail.com>
2019-05-03 12:22:12 +0000

    11.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token,:activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   
-  #before_save { self.email = email.downcase }
-  before_save { email.downcase! }
   validates :name,presence: true,length: {maximum:50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,presence: true,length: {maximum:255},
@@ -39,7 +39,18 @@ class User < ApplicationRecord
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
-  
 
+private
+
+  # メールアドレスを全て小文字にする
+  def downcase_email
+    self.email.downcase!
+  end
+  
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
   
 end
