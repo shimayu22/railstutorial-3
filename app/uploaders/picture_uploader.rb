9ab3b28a3d44018ebe4df4ac6a21463b2d35ebcb
Yutaka <shimayu22@gmail.com>
2019-06-18 13:31:41 +0000

    S3に保存する分岐を忘れたので追加

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,12 +1,16 @@
 class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-   include CarrierWave::MiniMagick
-   process resize_to_limit: [400, 400]
-
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+   
+  if Rails.env.production?
+    storage :fog
+  else
   # Choose what kind of storage to use for this uploader:
-  storage :file
+    storage :file
   # storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
